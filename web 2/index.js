0695665d2f70e2a8d203ca2459d19d16858f1c2e
MaldoNote <crismaldoalf@gmail.com>
2023-09-20 23:34:41 -0300

    first commit

---
+++ web 2/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const puerto = 80;
+
+app.listen(puerto, ()=>{
+    console.log(`Servidor corriendo en el puerto ${puerto}`)
+})
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Servidor EXPRESS desde el Contenedor 2</h1>`);
+});
+  
+
